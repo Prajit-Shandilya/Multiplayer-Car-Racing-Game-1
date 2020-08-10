b75604d7ed1ca90066bef2b7a60474949de59ac3
Prajit-Shandilya <66900497+Prajit-Shandilya@users.noreply.github.com>
2020-08-10 17:58:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var database, canvas;
+var form, player, game;
+var gameState = 0;
+var playerCount;
+
+function setup() {
+    canvas = createCanvas(500, 500);
+
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+
+}
+
+function draw() {
+    background("white");
+
+}
